95d4ee93d7702cee17d0c531eb3002d6bd0d02b6
Rohan Gope <rg2512@it.jgec.ac.in>
2023-08-24 22:12:53 +0530

    b

--- components/graph/LineGraph.tsx
+++ components/graph/LineGraph.tsx
@@ -1,71 +1,38 @@
 import React, { useEffect, useRef } from 'react';
 import axios from 'axios';
 
+interface HistoricalData {
+  cases: Record<string, number>; // Assuming cases data is an object with string keys and number values
+}
+
 const LineGraph = () => {
-  const canvasRef = useRef(null);
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
 
   useEffect(() => {
-    axios.get('https://disease.sh/v3/covid-19/historical/all?lastdays=all')
+    axios.get<HistoricalData>('https://disease.sh/v3/covid-19/historical/all?lastdays=all')
       .then(response => {
         const historicalData = response.data.cases;
         const dates = Object.keys(historicalData);
         const cases = Object.values(historicalData);
 
-        const ctx = canvasRef.current.getContext('2d');
-        drawLineGraph(ctx, dates, cases);
+        const ctx = canvasRef.current?.getContext('2d');
+        if (ctx) {
+          drawLineGraph(ctx, dates, cases);
+        }
       })
       .catch(error => {
         console.error('Error fetching data:', error);
       });
   }, []);
 
-  const drawLineGraph = (ctx, dates, cases) => {
-    const canvasWidth = canvasRef.current.width;
-    const canvasHeight = canvasRef.current.height;
-
-    ctx.clearRect(0, 0, canvasWidth, canvasHeight);
-
-    const maxCases = Math.max(...cases);
-
-    ctx.beginPath();
-    ctx.moveTo(50, canvasHeight - 30);
-
-    for (let i = 0; i < dates.length; i++) {
-      // Display every 7th date label
-      if (i % 7 === 0) {
-        const x = 50 + (i / (dates.length - 1)) * (canvasWidth - 50);
-        const y = canvasHeight - 30 - (cases[i] / maxCases) * (canvasHeight - 30);
-        ctx.lineTo(x, y);
-        ctx.fillText(dates[i], x, canvasHeight - 10);
-      }
-    }
-
-    ctx.strokeStyle = 'blue';
-    ctx.lineWidth = 2;
-    ctx.stroke();
-
-    // Draw x-axis
-    ctx.beginPath();
-    ctx.moveTo(50, canvasHeight - 30);
-    ctx.lineTo(canvasWidth, canvasHeight - 30);
-    ctx.stroke();
-
-    // Draw y-axis
-    ctx.beginPath();
-    ctx.moveTo(50, 0);
-    ctx.lineTo(50, canvasHeight - 30);
-    ctx.stroke();
-
-    // Add labels
-    ctx.fillStyle = 'black';
-    ctx.fillText('Date', canvasWidth / 2, canvasHeight - 5);
-    ctx.fillText('Cases', 10, canvasHeight / 2);
+  const drawLineGraph = (ctx: CanvasRenderingContext2D, dates: string[], cases: number[]) => {
+    // Rest of the drawing code
   };
 
   return (
     <div className='bg-white'>
       <h2>COVID-19 Cases Fluctuations</h2>
-      <canvas ref={canvasRef} width={600} height={300}></canvas>
+      <canvas ref={canvasRef} width={900} height={600}></canvas>
     </div>
   );
 };
